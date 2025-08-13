da907cf6e03dcd31a11973702cffaa675c1ccde6
SARA LAMARI SILVA <sara.silva591@etec.sp.gov.br>
2025-08-13 11:25:32 -0300

    feat: starting the server

---
+++ server.js
@@ -0,0 +1,23 @@
+import { sequelizeDatabase } from "./db.js"
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+(async () => {
+    try {
+        await sequelizeDatabase.sync();
+        console.log('Banco sincronizado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao sincronizar o banco:', error);
+    }
+})();
+
+app.get('/', (req, res) => res.send("API da Saphira rodando!"))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
