0291fa8e29a3aa1c34fb465bb3b2679eb60225bf
Bea-Xavier <bvinguerti@gmail.com>
2025-08-20 12:11:21 -0300

    Inicialização do Swagger e modificação de rotas Animais

--- src/controllers/animalsControllers.js
+++ src/controllers/animalsControllers.js
@@ -2,11 +2,22 @@ import Animal from "../models/Animal";
 
 export const GetAnimals = async (req, res) => {
     try {
+        //Terminar os filtros
+        //Fazendo o filtro de busca
+        const {especie, porte, castrado, vacinado, adotado} = req.query;
+        let filter = {};
+        if (especie) filter.especie = especie;
+        if (porte) filter.porte = porte;
+        if (castrado) filter.castrado = castrado === 'true';
+        if (vacinado) filter.vacinado = vacinado === 'true';
+
+        //Fazer ordenação de busca
+
         const animais = await Animal.findALL();
-        res.status(200).send({ message: 'Sucesso', animais });
+        res.status(201).send({ message: 'Sucesso', animais });
     } catch (error) {
         console.error({ message: 'Erro', error });
-        return res.status(500).json({ error: 'Erro inesperado' });
+        return res.status(500).json({ error: 'Erro ao buscar animais' });
     }
 };
 
