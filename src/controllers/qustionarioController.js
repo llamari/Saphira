c7546f915a6a37b6ed6feb2d306b25aeb79638e2
VITOR PAIVA NUNES DE PAULA <vitor.paula19@etec.sp.gov.br>
2025-08-27 11:28:47 -0300

    Final advances

--- src/controllers/qustionarioController.js
+++ src/controllers/qustionarioController.js
@@ -1,10 +1,12 @@
 import Questionario from "../models/Questionario";
 
-function fieldsVerifier(obj)  {
-    
-}
-
 export const postQuestionario = async(req,res) => {
     const body = req.body;
     
+    try {
+        await Questionario.create(body);
+        return res.status(200).send(body);
+    } catch (error) {
+        return res.status(500).send({"erro": "Erro ao registrar o pedido de adoção"});
+    }
 }
--- src/routes/questionario.routes.js
+++ src/routes/questionario.routes.js
@@ -1,8 +1,9 @@
 import { postQuestionario } from "../controllers/qustionarioController";
 import express from "express";
+import verificadorCamposForm from "../middlewares/verificaCamposForm";
 
 const questionario = express.Router()
 
-questionario.post('/questionario', postQuestionario);
+questionario.post('/questionario', verificadorCamposForm, postQuestionario);
 
 export default questionario;
