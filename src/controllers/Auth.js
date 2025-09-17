4afc84752245a644e1d52443557e6afc5bf4d5b1
Falkun <66378650+FelipeDinnouti@users.noreply.github.com>
2025-09-17 11:54:10 -0300

    feat: Auth middleware

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,11 +1,18 @@
 import Usuario from "../models/Usuario.js";
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
 export const login = async (req, res) => {
     try {
         const { email, senha } = req.body;
         const user = await Usuario.findOne({ where: { email: email } })
+
+        const username = user.username
+        const adminstrator = user.administrador
+
         if (user && user.senha === senha) {
-            res.send('Login bem sucedido')
+            const token = jwt.sign({ username, adminstrator }, JWT_SECRET, { expiresIn: "1h" });
+            res.send({"mensagem": 'Login bem sucedido', "token": token})
         } else {
             res.send({ "erro": "Email ou senha inválidos." })
         }
--- src/routes/animais.routes.js
+++ src/routes/animais.routes.js
@@ -1,8 +1,10 @@
 import e from "express";
 import { postAnimal } from "../controllers/animaisControllers.js";
+import { authenticate } from "../middlewares/authentication.js";
 
 const animais = e.Router();
 
+animais.use(authenticate)
 animais.post('/animais', e.json({ limit: '50mb' }), postAnimal);
 
 export default animais;
