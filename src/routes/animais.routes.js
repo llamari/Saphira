96958e953f359f3f5e75290d986e9718c7cee9d1
Falkun <66378650+FelipeDinnouti@users.noreply.github.com>
2025-09-30 08:41:20 -0300

    feat: protecting routes (unfinished)

--- src/routes/animais.routes.js
+++ src/routes/animais.routes.js
@@ -1,14 +1,10 @@
 import e from "express";
-import { deleteAnimal, GetAnimals, GetAnimalsId, listAnimals, postAnimal, updateAnimal } from "../controllers/animais.controller.js";
+import {GetAnimals, GetAnimalsId, postAnimal } from "../controllers/animais.controller.js";
 
 const animais = e.Router();
 
-animais.get('/admin/animais', listAnimals);
-animais.patch('/admin/animais/:id', updateAnimal);
-animais.delete('/admin/animais/:id', deleteAnimal);
-
-animais.post('/animais', e.json({ limit: '50mb' }), postAnimal);
-animais.get('/getAnimais', GetAnimals);
-animais.get('/getAnimais/:id', GetAnimalsId);
+animais.post('/', e.json({ limit: '50mb' }), postAnimal);
+animais.get('/', GetAnimals);
+animais.get('/:id', GetAnimalsId);
 
 export default animais;
