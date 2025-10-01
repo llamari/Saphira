e243d23d25e84eb13cc104df1fb2d6f57b94742d
Falkun <66378650+FelipeDinnouti@users.noreply.github.com>
2025-10-01 12:01:38 -0300

    feat: changing to Supabase

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelizeDatabase = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite',
+export const sequelizeDatabase = new Sequelize('postgres', 'postgres', 'HN=K_*&)3wvd\'w^', {
+    host: 'db.rnijhpbkaglkecafztog.supabase.co',
+    port: '5432',
+    dialect: 'postgres',
     logging: false,
 });
