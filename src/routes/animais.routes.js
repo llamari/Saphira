65618624cb887fee212c5b37ff069166d84a9f2a
Falkun <66378650+FelipeDinnouti@users.noreply.github.com>
2025-10-01 11:29:12 -0300

    fix: correct the animais:id route to be admin protected

--- src/routes/animais.routes.js
+++ src/routes/animais.routes.js
@@ -3,8 +3,8 @@ import {GetAnimals, GetAnimalsId, postAnimal } from "../controllers/animais.cont
 import { authenticate, authenticate_admin } from "../middlewares/authentication.js";
 const animais = e.Router();
 
-animais.post('/', authenticate, e.json({ limit: '50mb' }), postAnimal);
-animais.get('/', authenticate, GetAnimals);
-animais.get('/:id', authenticate_admin, GetAnimalsId);
+animais.use(authenticate)
+animais.post('/', e.json({ limit: '50mb' }), postAnimal);
+animais.get('/', GetAnimals);
 
 export default animais;
--- src/routes/animais_dashboard.routes.js
+++ src/routes/animais_dashboard.routes.js
@@ -1,12 +1,14 @@
 import e from "express";
-import { deleteAnimal, listAnimals, updateAnimal } from "../controllers/animais.controller.js";
+import { deleteAnimal, GetAnimalsId, listAnimals, updateAnimal } from "../controllers/animais.controller.js";
 import { authenticate_admin } from "../middlewares/authentication.js"
 
 const animais_admin = e.Router();
 
 // Protected Routes
 animais_admin.use(authenticate_admin);
+
 animais_admin.get('/', listAnimals);
+animais_admin.get('/:id', GetAnimalsId);
 animais_admin.patch('/:id', updateAnimal);
 animais_admin.delete('/:id', deleteAnimal);
 
